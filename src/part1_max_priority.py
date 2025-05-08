b1689669a6347a07158fec7d83a7230e122acf7e
Ahmet Furkan KIZIL <furkan.kizil@ug.bilkent.edu.tr>
2025-05-08 20:50:08 +0300

    Part 1 detailed commenting is done

--- src/part1_max_priority.py
+++ src/part1_max_priority.py
@@ -4,14 +4,21 @@ from gurobipy import GRB
 def solve_part1(seekers, jobs, compatible_pairs):
     model = gp.Model("Job_Matching_Priority")
 
+    # Decision Variable
+    # x[i,j] = 1 means seeker i is matched to job j.
     x = model.addVars([k for k in compatible_pairs if compatible_pairs[k]], vtype=GRB.BINARY, name="x")
 
+    # Objective Function
+    # gurobipy quickly sums up the Total Priority-Weights and maximizes it
     model.setObjective(gp.quicksum(jobs.loc[j, 'Priority_Weight'] * x[i, j]
                                    for (i, j) in x), GRB.MAXIMIZE)
 
+    # Less than <=1 constraint: Each seeker can be assigned to at most 1 job opening
     for i in seekers.index:
         model.addConstr(gp.quicksum(x[i, j] for (si, j) in x if si == i) <= 1)
 
+    # Second constraint: The number of seekers assigned to a job opening cannot exceed its available positions (Pj)
+    # This constraint ensures that the total number of seekers assigned to job j less than or equal to the job's capacity
     for j in jobs.index:
         model.addConstr(gp.quicksum(x[i, j] for (i, sj) in x if sj == j) <= jobs.loc[j, 'Num_Positions'])
 
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -13,16 +13,26 @@ def parse_data(seekers_path, jobs_path, distances_path):
 
     return seekers, jobs, location_distances
 
+# Fundamental requirements: All Fundamental Requirements are applied with this function for Part 1.
 def is_compatible(seeker, job, distance_matrix):
+    # Requirement 1 (Job Type): Seeker's desired type is compatible with the job's type, if not returns false
     if seeker['Desired_Job_Type'] != job['Job_Type']:
         return False
+    # Requirement 2 (Salary): Seeker's minimum desired salary alighns with the job's salary offering
+        # The logic is to compare the Seeker's Min_Desired_Salary with the Salary_Range_Max and return False if not.
+        # Traversing each possible job-seeker combinations in the check_compatibilty function automatically filters
+        # out the inappropriate ones from the model.
     if seeker['Min_Desired_Salary'] > job['Salary_Range_Max']:
         return False
+    # Requirement 3 (Skills):  Seeker possesses all skills required by the job. Exact match for required skills is ensured
     if not set(job['Required_Skills']).issubset(seeker['Skills']):
         return False
     levels = ['Entry-level', 'Mid-level', 'Senior', 'Lead', 'Manager']
+    # Requirement 4 (Experience): The experience levels are listed in levels array and simple index comparison is done satisfy.
     if levels.index(seeker['Experience_Level']) < levels.index(job['Required_Experience_Level']):
         return False
+    # Requirement 5 (Location): If the job is not remote, then the distance between the job's nominal locaiton is compared
+        # with the seeker's maximum commute distance. 
     if not job['Is_Remote']:
         distance = distance_matrix.loc[seeker['Location'], job['Location']]
         if distance > seeker['Max_Commute_Distance']:
